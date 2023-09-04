9b88953e5acb9a24e43f7d5d641f3dec2eb01dab
Lucas Krietenstein <lucas.krietenstein@wago.com>
2023-09-04 13:16:15 +0200

    Formatierung und Kommentare für Methoden

--- src/VierGewinnt/src/main.cpp
+++ src/VierGewinnt/src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include <LEDMatrix.h>
 
+// Pin-Definitionen für die LEDs und Tasten
 #define line1red 21
-#define line2red 15//3
-#define line3red 2//1
+#define line2red 15  //3
+#define line3red 2   //1
 #define line4red 22
 #define line5red 23
 
@@ -25,6 +26,7 @@
 #define tasterRst 35
 #define tasterU 32
 
+// Konstanten für die LED-Matrix
 const int nColumns = 6;
 const int nLines = 5;
 const int off = 0;
@@ -34,124 +36,116 @@ const int flashRed = 3;
 const int flashGreen = 4;
 const int startColor = red;
 
+// Variablen für den Spielstatus
 int lastButton = 0;
 bool player1 = HIGH;
 int currentColor = startColor;
 int currentColumn = 0;
-int LEDMatrixPins[nColumns*3] = {ground1, ground2, ground3, ground4, ground5, ground6,
-                                line1red, line2red, line3red, line4red, line5red, 0,
-                                line1green, line2green, line3green, line4green, line5green, 0};
+
+// Array für die Pin-Belegung der LED-Matrix
+int LEDMatrixPins[nColumns * 3] = {ground1, ground2, ground3, ground4, ground5, ground6,
+                                    line1red, line2red, line3red, line4red, line5red, 0,
+                                    line1green, line2green, line3green, line4green, line5green, 0};
 LEDMatrix lm(LEDMatrixPins, nColumns, nLines);
 
+// Funktion zum Auslesen der Tasten
 void readButtons(bool);
+// Funktion zum Zurücksetzen des Spiels
 void reset();
 
 void setup() {
-  pinMode(ground1,OUTPUT);
-  pinMode(ground2,OUTPUT);
-  pinMode(ground3,OUTPUT);
-  pinMode(ground4,OUTPUT);
-  pinMode(ground5,OUTPUT);
-  pinMode(ground6,OUTPUT);
-  
-  pinMode(line1red, OUTPUT);
-  pinMode(line2red, OUTPUT);
-  pinMode(line3red, OUTPUT);
-  pinMode(line4red, OUTPUT);
-  pinMode(line5red, OUTPUT);
-  
-  pinMode(line1green, OUTPUT);
-  pinMode(line2green, OUTPUT);
-  pinMode(line3green,OUTPUT);
-  pinMode(line4green, OUTPUT);
-  pinMode(line5green,OUTPUT);
+    // Konfiguration der Pins
+    pinMode(ground1, OUTPUT);
+    pinMode(ground2, OUTPUT);
+    pinMode(ground3, OUTPUT);
+    pinMode(ground4, OUTPUT);
+    pinMode(ground5, OUTPUT);
+    pinMode(ground6, OUTPUT);
 
-  pinMode(tasterL, INPUT);
-  pinMode(tasterR, INPUT);
-  pinMode(tasterU, INPUT);
-  pinMode(tasterRst, INPUT);
+    pinMode(line1red, OUTPUT);
+    pinMode(line2red, OUTPUT);
+    pinMode(line3red, OUTPUT);
+    pinMode(line4red, OUTPUT);
+    pinMode(line5red, OUTPUT);
 
-  Serial.begin(9600);
+    pinMode(line1green, OUTPUT);
+    pinMode(line2green, OUTPUT);
+    pinMode(line3green, OUTPUT);
+    pinMode(line4green, OUTPUT);
+    pinMode(line5green, OUTPUT);
+
+    pinMode(tasterL, INPUT);
+    pinMode(tasterR, INPUT);
+    pinMode(tasterU, INPUT);
+    pinMode(tasterRst, INPUT);
+
+    // Serielle Kommunikation starten
+    Serial.begin(9600);
 }
 
 void loop() {
-  readButtons(lm.update());
-  //lm.rollingStone(currentColumn, currentColor);
-  
-  delay(1);
+    // Tasten auslesen und Spielstatus aktualisieren
+    readButtons(lm.update());
+    delay(1);
 }
 
-void readButtons(bool won){
-  
-  if(digitalRead(tasterL)){
-    if(currentColumn != 0 && lastButton != tasterL && currentColumn != -1 && !won)
-    {
-      int pos = lm.findPossibleDestination(currentColumn-1, -1);
-      if(pos >= 0){
-        lastButton = tasterL;
-        if(currentColor == red){
-          lm.setLightValue(pos, currentColumn, flashRed);
+void readButtons(bool won) {
+    // Tastenabfrage und Aktionen je nach gedrückter Taste
+    if (digitalRead(tasterL)) {
+        if (currentColumn != 0 && lastButton != tasterL && currentColumn != -1 && !won) {
+            int pos = lm.findPossibleDestination(currentColumn - 1, -1);
+            if (pos >= 0) {
+                lastButton = tasterL;
+                if (currentColor == red) {
+                    lm.setLightValue(pos, currentColumn, flashRed);
+                } else {
+                    lm.setLightValue(pos, currentColumn, flashGreen);
+                }
+                currentColumn = pos;
+            }
         }
-        else{
-          lm.setLightValue(pos, currentColumn, flashGreen);
+    } else if (digitalRead(tasterR)) {
+        if (currentColumn != nColumns - 1 && lastButton != tasterR && currentColumn != -1 && !won) {
+            int pos = lm.findPossibleDestination(currentColumn + 1, 1);
+            if (pos >= 0) {
+                lastButton = tasterR;
+                if (currentColor == red) {
+                    lm.setLightValue(pos, currentColumn, flashRed);
+                } else {
+                    lm.setLightValue(pos, currentColumn, flashGreen);
+                }
+                currentColumn = pos;
+            }
         }
-        currentColumn = pos;
-      }
+    } else if (digitalRead(tasterU)) {
+        if (lastButton != tasterU && currentColumn != -1 && !won) {
+            lastButton = tasterU;
+            lm.dropDown = true;
+            lm.setLightValue(currentColumn, currentColumn, currentColor);
+            player1 = !player1;
+            if (player1) {
+                currentColor = startColor;
+            } else {
+                currentColor = green;
+            }
+            currentColumn = lm.findPossibleDestination(0, 1);
+        }
+    } else if (digitalRead(tasterRst)) {
+        reset();
+    } else if (!won) {
+        lastButton = 0;
+        if (currentColor == red) {
+            lm.setLightValue(currentColumn, currentColumn, flashRed);
+        } else {
+            lm.setLightValue(currentColumn, currentColumn, flashGreen);
+        }
     }
-  }
-  else if(digitalRead(tasterR)){
-    
-    if(currentColumn != nColumns-1 && lastButton != tasterR && currentColumn != -1 && !won)
-    {
-      int pos = lm.findPossibleDestination(currentColumn+1, 1);
-      if(pos >= 0)
-      {
-        lastButton = tasterR;
-        if(currentColor == red){
-          lm.setLightValue(pos, currentColumn, flashRed);
-        }
-        else{
-          lm.setLightValue(pos, currentColumn, flashGreen);
-        }
-        currentColumn = pos;
-      }
-    }
-    
-  }
-  else if(digitalRead(tasterU)){
-    if(lastButton != tasterU && currentColumn != -1 && !won)
-    {
-      lastButton = tasterU;
-      lm.dropDown = true;
-      lm.setLightValue(currentColumn, currentColumn, currentColor);
-      player1 = !player1;
-      if(player1){
-        currentColor = startColor;
-      }
-      else{
-        currentColor = green;
-      }
-      currentColumn = lm.findPossibleDestination(0, 1);
-      //Serial.println((int)currentColumn);
-    }
-  }
-  else if(digitalRead(tasterRst)){
-    reset();
-  }
-  else if(!won){
-    lastButton = 0;
-    if(currentColor == red){
-        lm.setLightValue(currentColumn, currentColumn, flashRed);
-      }
-      else{
-        lm.setLightValue(currentColumn, currentColumn, flashGreen);
-      }
-  }
 }
 
-void reset(){
-  lm.reset();
-  player1 = HIGH;
-  currentColor = startColor;
-  currentColumn = 0;
+void reset() {
+    // Spiel zurücksetzen
+    lm.reset();
+    player1 = HIGH;
+    currentColor = startColor;
+    currentColumn = 0;
 }
