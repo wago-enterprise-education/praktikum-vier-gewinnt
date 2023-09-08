7841ed66b3b7f7f64505bee49548a0f6ea843a41
Andreas Epp <andreas.epp@wago.com>
2023-09-08 09:42:48 +0200

    Add Algorithm

---
+++ src/VierGewinnt/src/miniMax.h
@@ -0,0 +1,31 @@
+#ifndef MINIMAX_H
+#define MINIMAX_H
+
+#include <Arduino.h>
+#include <vector>
+
+class miniMax
+{
+    public:
+        std::vector<int> run(std::vector <std::vector<int>>);
+        int Algorithm(std::vector<std::vector<int>>, bool);
+
+    private:
+        int nColumns = 6;     //Muss noch variabel gehalten werden
+        int nRows = 5;        //...
+        const int nothingFound = -10;
+        const int win = 1;
+        const int draw = 0;
+        const int lose = -1;
+        const int red = 1;
+        const int green = 2;
+
+        bool columnEmpty(std::vector<std::vector<int>>, int);
+        int findPossibleDestination(std::vector<std::vector<int>>, int);
+        bool winControl(std::vector<std::vector<int>>);
+        bool drawControl(std::vector<std::vector<int>>);
+        bool lostControl(std::vector<std::vector<int>>);
+        int gameOver(std::vector<std::vector<int>>);
+};
+
+#endif
