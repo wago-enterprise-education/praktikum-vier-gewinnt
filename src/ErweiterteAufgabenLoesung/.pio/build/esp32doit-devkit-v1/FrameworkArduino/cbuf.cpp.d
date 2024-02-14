c787265fa75dbca8531df85eca2f8ed95d9cc6a8
Andreas Epp <andreas.epp@wago.com>
2024-02-14 13:20:00 +0100

    kleines Update

--- src/ErweiterteAufgabenLoesung/.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/U0127626/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/U0127626/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
